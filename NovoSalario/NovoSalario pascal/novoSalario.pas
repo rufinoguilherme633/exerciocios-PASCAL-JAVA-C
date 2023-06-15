cce6b4443eef00048af56a33df78ec49a4228be4
GUILHERME RUFINO CAMPOS <22102603@CFP127ALUNO.LOCAL>
2023-06-15 16:56:54 -0300

    Novo salario for

--- NovoSalario/NovoSalario pascal/novoSalario.pas
+++ NovoSalario/NovoSalario pascal/novoSalario.pas
@@ -1,16 +1,23 @@
-                                                                              program NovoSalario;
-                                                                              uses crt;
-                                                                              var i,salario,novo_salario,percentual: real;
-                                                                              ano_atual:integer;
+                                             program NovoSalario;
+                                             uses crt;
+                                             var salario,novo_salario,percentual:real;
+                                             i,ano_atual:integer;
+                                             begin
+                                             writeln('Digite o ano');
+                                             readln(ano_atual);
+                                             salario:=1000;
+                                             percentual:=1.5/100;
+                                             writeln(salario:7:2);
+                                             writeln(percentual:7:2);
+                                             for i:= 2007 to ano_atual do
+                                             begin
+                                             novo_salario:= salario + (salario * percentual);   ;
+                                             percentual:= 2 * percentual;
+                                             salario:= novo_salario;
+                                             end;
+                                             writeln('Percentual ', percentual:7:3);
+                                             writeln('novo salario ',novo_salario:7:2);
+                                             writeln('----------------');
 
-                                                                              begin
-
-                                                                              writeln('Digite o ano atual');
-                                                                              readln(ano_atual);
-                                                                             // writeln(ano_atual);
-                                                                             salario:= 1000;
-                                                                             percentual:=   1.5/100;
-                                                                             writeln(salario:7:2);
-                                                                             writeln(percentual:7:3);
-                                                                              readln;
-                                                                              end.
+                                            readln;
+                                             end.
