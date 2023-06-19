4311e221c2226343374df60e3ba31c8ce5d51e78
GUILHERME RUFINO CAMPOS <22282090@CFP127ALUNO.LOCAL>
2023-06-19 11:20:17 -0300

    NOvoSalarioBonificacaoAuxiolio java inicio

---
+++ NovoSalarioBonificacao/NovosalarioBonificacao java/NovoSalarioBonificacao.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class NovoSalarioBonificacaoEscola {
+
+    public static void main(String args[]) {
+        float salario, novo_salario, bonificacao, auxilio;
+        Scanner dado = new Scanner(System.in);
+        System.out.print("Digite o salário atual: ");
+        salario = dado.nextFloat();
+        System.out.println("Salário informado: " + salario);
+
+        if (salario <= 600) {
+            auxilio = 150;
+            if (salario <= 500) {
+                bonificacao = salario * (5.0f / 100);
+                novo_salario = salario + auxilio + bonificacao;
+            } else {
+                bonificacao = 0;
+                novo_salario = salario + auxilio;
+            }
+
+            System.out.println("Novo salário: " + novo_salario);
+        } else {
+            auxilio = 100;
+            novo_salario = salario + auxilio;
+            System.out.println("Novo salário: " + novo_salario);
+        }
+    }
+}
