b4f35e75265c1293bcef554b1097fd0879c4093f
GUILHERME RUFINO CAMPOS <22282090@CFP127ALUNO.LOCAL>
2023-06-20 10:48:08 -0300

    terminno novo salario em C++

--- NovoSalario/NovoSalario C++/novoSalario.cpp
+++ NovoSalario/NovoSalario C++/novoSalario.cpp
@@ -6,34 +6,46 @@ C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
+//page 1
+//. Um funcionário de uma empresa recebe, anualmente, aumento salarial. Sabe-se que: 
+//a) Esse funcionário foi contratado em 2005, com salário inicial de R$ 1.000,00. 
+//b) Em 2006, ele recebeu aumento de 1,5% sobre seu salário inicial. 
+//c) A partir de 2007 (inclusive), os aumentos salariais sempre corresponderam ao dobro do percentual do ano 
+//anterior. 
+//Faça um programa que determine o salário atual desse funcionário.
+
 #include <stdio.h>
 
 int main() {
     float salario, novo_salario, percentual;
     int i, ano_atual;
+    
     printf("Digite o ano atual: ");
     scanf("%d", &ano_atual);
+    
     salario = 1000;
     percentual = 1.5 / 100;
-    printf("%f\n", percentual);
-    printf("%f\n salario", salario);
-    printf("ano digitado: %d\n", ano_atual);
-    if (ano_atual != 0) {
+    
+    printf("Percentual inicial: %.2f\n", percentual);
+    printf("Salário inicial (2007): %.2f\n", salario);
+    
+    if (ano_atual >= 2007) {
         for (i = 2007; i <= ano_atual; i++) {
-            percentual = percentual * 2;
             novo_salario = salario + (salario * percentual);
             salario = novo_salario;
-              printf("\n----------------------\n");
-              printf("ano %i\n", i);
-              printf("ano %f\n", percentual);
-            printf("salario do ano: %f\n", salario);
+            
+            printf("\n----------------------\n");
+            printf("Ano: %d\n", i);
+            printf("Percentual: %.2f\n", percentual);
+            printf("Salário do ano: %.2f\n", salario);
+            
+            percentual = percentual * 2; // Aumento do percentual para o próximo ano
         }
-        printf("salario atual: %f\n", novo_salario);
+        printf("\nSalário atual: %.2f\n", novo_salario);
     }
     else {
-        printf("Não foi possível calcular o salário\n");
+        printf("\nNão foi possível calcular o salário\n");
     }
+    
     return 0;
 }
-
-
